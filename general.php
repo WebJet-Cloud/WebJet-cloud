e648eda238d806a139de9f655d6218d13e8694c8
AlexonbStudio <65239996+alexonbstudio@users.noreply.github.com>
2023-06-02 18:28:59 +0200

    Update general.php

--- general.php
+++ general.php
@@ -119,16 +119,89 @@ if(isset($_GET['lang'])){
 				include_once('themes/'.$sites['template'].'/general/aboutus.php');
 				include('themes/'.$sites['template'].'/footer.php');	
 			} else if($_GET['pages'] == 'services'){
-				$title = $general['services']['title'];
-				$description = $general['services']['description'];
-				$keyword = $general['services']['keyword'];
-				$urls = $general['services']['url'][$DefineTranslateLang];
-				$imgs = $general['services']['sitemap']['images'];
-				$vdos = $general['services']['sitemap']['video'];
-				define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$general['services']['url'][$DefineTranslateLang]);
-				include('themes/'.$sites['template'].'/header.php');
-				include_once('themes/'.$sites['template'].'/general/services.php');
-				include('themes/'.$sites['template'].'/footer.php');	
+				if(isset($_GET['activities'])){
+					if($_GET['activities'] == 'cybersecurity'){
+						$title = $services['cybersecurity']['title'];
+						$description = $services['cybersecurity']['description'];
+						$keyword = $services['cybersecurity']['keyword'];
+						$urls = $services['cybersecurity']['url'][$DefineTranslateLang];
+						$imgs = $services['cybersecurity']['sitemap']['images'];
+						$vdos = $services['cybersecurity']['sitemap']['video'];
+						define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$services['cybersecurity']['url'][$DefineTranslateLang]);
+						include('themes/'.$sites['template'].'/header.php');
+						include_once('themes/'.$sites['template'].'/services/cybersecurity.php');
+						include('themes/'.$sites['template'].'/footer.php');	
+					} else if($_GET['activities'] == 'marketing'){
+						$title = $services['marketing']['title'];
+						$description = $services['marketing']['description'];
+						$keyword = $services['marketing']['keyword'];
+						$urls = $services['marketing']['url'][$DefineTranslateLang];
+						$imgs = $services['marketing']['sitemap']['images'];
+						$vdos = $services['marketing']['sitemap']['video'];
+						define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$services['marketing']['url'][$DefineTranslateLang]);
+						include('themes/'.$sites['template'].'/header.php');
+						include_once('themes/'.$sites['template'].'/services/marketing.php');
+						include('themes/'.$sites['template'].'/footer.php');	
+					} else if($_GET['activities'] == 'webmaster'){
+						$title = $services['webmaster']['title'];
+						$description = $services['webmaster']['description'];
+						$keyword = $services['webmaster']['keyword'];
+						$urls = $services['webmaster']['url'][$DefineTranslateLang];
+						$imgs = $services['webmaster']['sitemap']['images'];
+						$vdos = $services['webmaster']['sitemap']['video'];
+						define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$services['webmaster']['url'][$DefineTranslateLang]);
+						include('themes/'.$sites['template'].'/header.php');
+						include_once('themes/'.$sites['template'].'/services/webmaster.php');
+						include('themes/'.$sites['template'].'/footer.php');	
+					} else if($_GET['activities'] == 'developper'){
+						$title = $services['developper']['title'];
+						$description = $services['developper']['description'];
+						$keyword = $services['developper']['keyword'];
+						$urls = $services['developper']['url'][$DefineTranslateLang];
+						$imgs = $services['developper']['sitemap']['images'];
+						$vdos = $services['developper']['sitemap']['video'];
+						define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$services['developper']['url'][$DefineTranslateLang]);
+						include('themes/'.$sites['template'].'/header.php');
+						include_once('themes/'.$sites['template'].'/services/developper.php');
+						include('themes/'.$sites['template'].'/footer.php');	
+					} else if($_GET['activities'] == 'seo'){
+						$title = $services['seo']['title'];
+						$description = $services['seo']['description'];
+						$keyword = $services['seo']['keyword'];
+						$urls = $services['seo']['url'][$DefineTranslateLang];
+						$imgs = $services['seo']['sitemap']['images'];
+						$vdos = $services['seo']['sitemap']['video'];
+						define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$services['seo']['url'][$DefineTranslateLang]);
+						include('themes/'.$sites['template'].'/header.php');
+						include_once('themes/'.$sites['template'].'/services/seo.php');
+						include('themes/'.$sites['template'].'/footer.php');	
+					} else if($_GET['activities'] == 'consulting'){
+						$title = $services['consulting']['title'];
+						$description = $services['consulting']['description'];
+						$keyword = $services['consulting']['keyword'];
+						$urls = $services['consulting']['url'][$DefineTranslateLang];
+						$imgs = $services['consulting']['sitemap']['images'];
+						$vdos = $services['consulting']['sitemap']['video'];
+						define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$services['consulting']['url'][$DefineTranslateLang]);
+						include('themes/'.$sites['template'].'/header.php');
+						include_once('themes/'.$sites['template'].'/services/consulting.php');
+						include('themes/'.$sites['template'].'/footer.php');
+					} else {
+						header('Location: '.$protocols.'://'.$domainTLD.'/'.$general['services']['url'][$DefineTranslateLang]);
+					}
+				} else {
+					$title = $general['services']['title'];
+					$description = $general['services']['description'];
+					$keyword = $general['services']['keyword'];
+					$urls = $general['services']['url']['default'];
+					$imgs = $general['services']['sitemap']['images'];
+					$vdos = $general['services']['sitemap']['video'];
+					define('__WP_'.$DefineMajLang.'_URL', $DefineTranslateLang.'/'.$general['services']['url'][$DefineTranslateLang]);
+					include('themes/'.$sites['template'].'/header.php');
+					include_once('themes/'.$sites['template'].'/general/services.php');
+					include('themes/'.$sites['template'].'/footer.php');
+				}
+		
 			} else if($_GET['pages'] == 'teams'){
 				$title = $teams['index']['title'];
 				$description = $teams['index']['description'];
